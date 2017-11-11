a3a002ce90558a112e4aa3979dfb7b61ea3b872f
Kenneth Madsen <mail@kenneth.eu>
2017-11-11 22:38:01 +0100

    First upload

---
+++ app.js
@@ -0,0 +1,86 @@
+// Calc Controller
+var calcController = (function() {
+
+    return {
+        getinput: function() {
+
+        }
+    }
+
+})();
+
+// UI Controller
+var UIController = (function() {
+
+
+})();
+
+document.querySelector('.beregn_Score').addEventListener('click', function() {
+   beregnScore();
+})
+
+var score = 0;
+var notchecked = 0;
+
+var beregnScore = function() {
+    var sporgsmal = ['q1', 'q2', 'q3', 'q4'];
+
+    score = 0;
+    notchecked = 0;
+
+    for (var sp = 0, length = sporgsmal.length; sp < length; sp++) {
+
+        var q = document.getElementsByName(sporgsmal[sp]);
+        
+            for (var i = 0, qlength = q.length; i < qlength; i++) {
+                if (q[i].checked) {
+                    // do whatever you want with the checked radio
+                    console.log(q[i].value);
+                    if (q[i].value === 'na') {
+                        notchecked++;
+                    } else {
+                    score += parseInt(q[i].value);
+                    }
+                    
+                } else {
+                    notchecked++;
+                }
+            }
+    }
+    notchecked = notchecked - 14;
+    adj = 0; 
+    if (notchecked === 3) {
+        adj = 4;
+    } if (notchecked === 2) {
+        adj = 2;
+    } if (notchecked === 1) {
+        adj = 16/12;
+    } if (notchecked === 0) {
+        adj = 1;
+    }
+    adjscore = score * adj;
+    console.log('Din score er ' + score + ' du har ikke svaret på ' + notchecked + ' din justerede score er derfor ' + adjscore + ' justeringsfator er ' + adj);
+    document.querySelector('.din_score').textContent='Din fordelingspolitisk score er ' + adjscore + ' på en scale fra -16 til 16, hvor -16 er meget venstreorienteret og 16 er meget højreorienteret. Nedenfor kan du også se din placering på den fordelingspolitiske akse, du er den hvide streg.';
+    document.querySelector('.din_fordeling').classList.add('rbbg');
+    procentscore = ((adjscore + 16) * 3.125) - 1.5;
+    document.querySelector('.din_fordeling').setAttribute("style", "padding-left:" + procentscore + "% !important;");
+    return adjscore;
+
+}
+
+
+
+/*
+    var q2 = document.getElementsByName('q2');
+    
+        for (var i = 0, length = q1.length; i < length; i++) {
+            if (q2[i].checked) {
+                // do whatever you want with the checked radio
+                console.log(q2[i].value);
+                score.push(parseInt(q2[i].value));
+    
+                // only one radio can be logically checked, don't check the rest
+                break;
+            }
+        }
+*/
